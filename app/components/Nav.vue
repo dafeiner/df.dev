addc1907020a2451de03ffb143c1df4e0c419538
David Feinerman <david.feinerman@clutter.com>
2020-06-15 22:50:55 -0700

    Build responsive nav
    - Learned to return data as function
    - Added methods
    - Event handlers in button

--- app/components/Nav.vue
+++ app/components/Nav.vue
@@ -4,7 +4,8 @@
   >
     <div class="block lg:hidden">
       <button
-        class="flex items-center px-3 py-2 border rounded border-teal-400 hover:text-white hover:border-white"
+        v-on:click="toggle"
+        class="flex items-center px-3 py-2 border rounded border-purple-900"
       >
         <svg
           class="fill-current h-3 w-3"
@@ -12,12 +13,13 @@
           xmlns="http://www.w3.org/2000/svg"
         >
           <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          <path style="stroke: #44337a; fill: #44337a" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
       </button>
     </div>
     <div
-      class="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-lg"
+      :class="collapsed ? 'hidden': 'block'"
+      class="w-full flex-grow lg:flex sm:items-center lg:w-auto text-lg"
     >
       <div class="text-sm lg:flex-grow">
         <nuxt-link
@@ -36,3 +38,18 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapsed: true
+    }
+  },
+  methods: {
+    toggle() {
+      this.collapsed = !this.collapsed
+    }
+  }
+}
+</script>
