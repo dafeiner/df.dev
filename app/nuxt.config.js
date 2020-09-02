8023b749e3cba460abef2b0c1e5512ff41006ca7
David Feinerman <david.feinerman@clutter.com>
2020-09-02 00:27:19 -0700

    Add lang attribute to html tag
    Lighthouse score complains if no lang attribute is present

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -5,6 +5,7 @@ export default {
    */
   head: {
     title: 'df.dev',
+    lang: 'en',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
