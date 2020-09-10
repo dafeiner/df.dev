843da1d5c92ae78972625c8df7f265724183f0be
David Feinerman <david.feinerman@clutter.com>
2020-09-10 00:12:24 -0700

    Add very basic about me/home page
    - Contains contact info

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -15,7 +15,11 @@ export default {
         content: 'Personal website of David Feinerman'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Philosopher&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Catamaran&family=Source+Code+Pro&display=swap' }
+    ]
   },
   /*
    ** Customize the progress-bar color
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,46 +1,41 @@
 <template>
-  <div class="container">
+  <div class="container max-w-2xl mx-auto text-lext flex items-center px-4">
     <div>
-      <h1 class="title">
-        df.dev
+      <h1 class="title text-4xl md:text-6xl sm:text-4xl">
+        David Feinerman
       </h1>
-      <h2 class="subtitle">
-        Personal website of David Feinerman
-      </h2>
+      <p class="text-2xl">
+        Welcome to my page. I am software builder out of Los Angeles, currently
+        at <a target="_blank" href="https://clutter.com">Clutter.</a>
+      </p>
+      <p>
+        This website is a sandbox of my ideas, projects, learnings and failures.
+      </p>
+      <p>
+        You can reach me on <a target="_blank" href="https://twitter.com/dafeiner">Twitter</a> or
+        <a target="_blank" href="https://www.linkedin.com/in/david-feinerman/">LinkedIn</a> or
+        <a type="email" href="mailto:fire@hey.com">email</a> me.
+      </p>
     </div>
   </div>
 </template>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
+<style scoped>
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Philosopher', sans-serif, 'Source Sans Pro', -apple-system,
+    BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
+p {
+  @apply text-2xl mb-3;
+  font-family: 'Catamaran';
   color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+}
+
+a {
+  @apply underline;
 }
 </style>
