3f83712ce0330137cb462bea87ef01fa45fa40d0
David Feinerman <david.feinerman@clutter.com>
2021-01-03 13:04:56 -0800

    Run prettier

--- app/plugins/ghost.ts
+++ app/plugins/ghost.ts
@@ -1,15 +1,18 @@
-import GhostContentAPI from "@tryghost/content-api";
+import GhostContentAPI from '@tryghost/content-api';
 
 // Create API instance with site credentials
 const api = new GhostContentAPI({
   url: 'https://davidfeinerman.ghost.io',
   key: process.env.ghostAPIKey!,
-  version: "v3"
+  version: 'v3'
 });
 
 export async function getPosts() {
   return await api.posts
     .browse({
-      limit: "all",
-    }).catch((err: any) => console.error(err))
+      limit: 'all'
+    })
+    .catch((err: any) => {
+      throw err;
+    });
 }
