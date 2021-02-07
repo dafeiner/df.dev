f18b999d24f89600524ad417966e5f15eccce221
David Feinerman <david.feinerman@clutter.com>
2021-02-06 18:02:26 -0800

    Update accent color to indigo 700

--- app/components/Nav.vue
+++ app/components/Nav.vue
@@ -1,11 +1,11 @@
 <template>
   <nav
-    class="flex items-center justify-between flex-wrap p-6 border-b-2 border-purple-900"
+    class="flex items-center justify-between flex-wrap p-6 border-b-2 border-indigo-700"
   >
     <div class="block lg:hidden">
       <button
         v-on:click="toggle"
-        class="flex items-center px-3 py-2 border rounded border-purple-900"
+        class="flex items-center px-3 py-2 border rounded border-indigo-700"
       >
         <svg
           class="fill-current h-3 w-3"
@@ -13,23 +13,26 @@
           xmlns="http://www.w3.org/2000/svg"
         >
           <title>Menu</title>
-          <path style="stroke: #44337a; fill: #44337a" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          <path
+            style="stroke: #44337a; fill: #44337a"
+            d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"
+          />
         </svg>
       </button>
     </div>
     <div
-      :class="collapsed ? 'hidden': 'block'"
+      :class="collapsed ? 'hidden' : 'block'"
       class="w-full flex-grow lg:flex sm:items-center lg:w-auto text-lg"
     >
       <div class="text-sm lg:flex-grow">
         <nuxt-link
           to="/"
-          class="block mt-4 lg:inline-block lg:mt-0 text-purple-900 hover:underline mr-4"
+          class="block mt-4 lg:inline-block lg:mt-0 text-indigo-700 hover:underline mr-4"
         >
           df.dev
         </nuxt-link>
         <nuxt-link
-          class="block mt-4 lg:inline-block lg:mt-0 text-purple-900 hover:underline mr-4"
+          class="block mt-4 lg:inline-block lg:mt-0 text-indigo-700 hover:underline mr-4"
           to="/blog"
         >
           Blog
@@ -44,12 +47,12 @@ export default {
   data() {
     return {
       collapsed: true
-    }
+    };
   },
   methods: {
     toggle() {
-      this.collapsed = !this.collapsed
+      this.collapsed = !this.collapsed;
     }
   }
-}
+};
 </script>
